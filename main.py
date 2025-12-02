fb0546a9e29a268fd60240a8af2ea4dfdc88d036
Veikrys <al.cherr04@yandex.ru>
2025-12-02 15:28:02 +0300

    base of gameplay

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 #частота обновления экрана
-FPS = 60
+FPS = 45
 clock = pygame.time.Clock()
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 200
@@ -25,8 +25,8 @@ cpu_paddle = pygame.Rect(WIDTH - 50 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT 
 
 BALL_SIZE = 20
 ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
-ball_speed_x = 5
-ball_speed_y = 5
+ball_speed_x = 10
+ball_speed_y = 10
 
 #игровой цикл
 while True:
@@ -47,6 +47,35 @@ while True:
     if keys[pygame.K_DOWN] and player_paddle.bottom < HEIGHT:
         player_paddle.y += PADDLE_SPEED
 
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_speed_y *= -1.1
+
+    if ball.colliderect(player_paddle) or ball.colliderect(cpu_paddle):
+        ball_speed_x *= -1.1
+
+    if ball.left <= 0:
+        ball.center = (WIDTH // 2, HEIGHT // 2)
+        ball_speed_x = 10
+        ball_speed_y = 10
+        ball_speed_x *= +1.5
+    
+    if ball.right >= WIDTH:
+        ball.center = (WIDTH // 2, HEIGHT // 2)
+        ball_speed_x = 10
+        ball_speed_y = 10
+        ball_speed_x *= -1.5
+
+    if cpu_paddle.centery < ball.centery and cpu_paddle.bottom < HEIGHT:
+        cpu_paddle.y += PADDLE_SPEED * 0.85 
+
+    if cpu_paddle.centery > ball.centery and cpu_paddle.top > 0:
+        cpu_paddle.y -= PADDLE_SPEED * 0.85
+    
+    
+
     screen.fill(BLACK)
     pygame.draw.rect(screen, WHITE, player_paddle, 5)
     pygame.draw.rect(screen, WHITE, cpu_paddle, 5)
@@ -56,3 +85,4 @@ while True:
 
     pygame.display.update()
     clock.tick(FPS)
+
