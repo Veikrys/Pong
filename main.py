1d59de9f9a77f9933b24eff639ccb1d7d066a127
Veikrys <al.cherr04@yandex.ru>
2025-12-02 15:02:25 +0300

    base of base 3

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ BLACK = (0, 0, 0)
 FPS = 60
 clock = pygame.time.Clock()
 
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 200
+PADDLE_SPEED = 10
+
+player_paddle = pygame.Rect(50, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+cpu_paddle = pygame.Rect(WIDTH - 50 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+BALL_SIZE = 20
+ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
+ball_speed_x = 5
+ball_speed_y = 5
+
 #игровой цикл
 while True:
     for event in pygame.event.get():
@@ -25,12 +36,23 @@ while True:
             sys.exit()
 
     
+    # screen.fill(BLACK)
+
+    # pygame.display.update()
+    # clock.tick(FPS)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and player_paddle.top > 0:
+        player_paddle.y -= PADDLE_SPEED
+    if keys[pygame.K_DOWN] and player_paddle.bottom < HEIGHT:
+        player_paddle.y += PADDLE_SPEED
+
     screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, player_paddle, 5)
+    pygame.draw.rect(screen, WHITE, cpu_paddle, 5)
+    pygame.draw.ellipse(screen, WHITE, ball, 5)
+
+    pygame.draw.aaline(screen, WHITE, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), True)
 
     pygame.display.update()
     clock.tick(FPS)
-
-PADDLE_WITH, PADDLE_HEIGHT = 20, 200
-PADDLE_SPEED = 10
-
-player_paddle = pygame.Rect(50, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
