3258bc99e910551543610f32aa8e10ee1f57d682
Veikrys <al.cherr04@yandex.ru>
2025-12-02 16:09:53 +0300

    base of gameplay 2

--- main.py
+++ main.py
@@ -8,25 +8,33 @@ pygame.init()
 WIDTH, HEIGHT = 1280, 720
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong 1972")
+window_center = pygame.display.get_surface().get_rect().center
 
 #цвета
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREY = (130, 130, 130)
+font = pygame.font.SysFont("Comic Sans MS", 64)
 
 #частота обновления экрана
-FPS = 45
+FPS = 60
 clock = pygame.time.Clock()
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 200
-PADDLE_SPEED = 10
+PADDLE_SPEED = 15
 
 player_paddle = pygame.Rect(50, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
 cpu_paddle = pygame.Rect(WIDTH - 50 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
 BALL_SIZE = 20
 ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
-ball_speed_x = 10
-ball_speed_y = 10
+ball_speed_x = 5
+ball_speed_y = 5
+
+player_score = 0
+cpu_score = 0
+winning_score = 3
+game_over = False
 
 #игровой цикл
 while True:
@@ -57,15 +65,17 @@ while True:
         ball_speed_x *= -1.1
 
     if ball.left <= 0:
+        cpu_score += 1
         ball.center = (WIDTH // 2, HEIGHT // 2)
-        ball_speed_x = 10
-        ball_speed_y = 10
+        ball_speed_x = 5
+        ball_speed_y = 5
         ball_speed_x *= +1.5
     
     if ball.right >= WIDTH:
+        player_score += 1
         ball.center = (WIDTH // 2, HEIGHT // 2)
-        ball_speed_x = 10
-        ball_speed_y = 10
+        ball_speed_x = 5
+        ball_speed_y = 5
         ball_speed_x *= -1.5
 
     if cpu_paddle.centery < ball.centery and cpu_paddle.bottom < HEIGHT:
@@ -74,14 +84,19 @@ while True:
     if cpu_paddle.centery > ball.centery and cpu_paddle.top > 0:
         cpu_paddle.y -= PADDLE_SPEED * 0.85
     
-    
+    if player_score >= winning_score or cpu_score >= winning_score:
+        game_over = True
 
     screen.fill(BLACK)
     pygame.draw.rect(screen, WHITE, player_paddle, 5)
     pygame.draw.rect(screen, WHITE, cpu_paddle, 5)
-    pygame.draw.ellipse(screen, WHITE, ball, 5)
 
     pygame.draw.aaline(screen, WHITE, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), True)
+
+    score_text = font.render(f"| {player_score} : {cpu_score} |", True, WHITE, GREY)
+    screen.blit(score_text, (window_center[0] - score_text.get_width() // 2, window_center[1] // 10))
+
+    pygame.draw.ellipse(screen, WHITE, ball, 5)
 
     pygame.display.update()
     clock.tick(FPS)
